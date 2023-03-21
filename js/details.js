0244ce560f843ee83fd91022b9cb37ee5dd30d5e
IrisNieto9 <iris.nieto.montoya@mi.unc.edu.ar>
2023-03-21 02:42:43 -0300

    Creacion de stats.js e implementacion. Cambios en pagina Details. Detalles esteticos

--- js/details.js
+++ js/details.js
@@ -15,29 +15,33 @@ async function getData () {
     let currentDate = new Date(response.currentDate);
     let eventDate = new Date(events.date);
     if (eventDate < currentDate) {
-        asistenciaPast = `<p class="detail-card card-text text-center">Assistance: ${events.assistance}</p>`;
+        asistenciaPast = `<p class="detail-card card-text text-center"><b>Assistance:</b> ${events.assistance}</p>`;
+    }else if (eventDate > currentDate) {
+      asistenciaPast = `<p><b>Estimate:</b> ${events.estimate}</p>`;
     }
 
 
 
     let detailContainer = document.getElementById("details-container")
 
-    let detailCardOpen=  `
-      <div class="card m-5 p-4 mb-6 border-danger-subtle align-content-center">
-          <div class="flex-row g-0 d-flex m-4" style="width: 50vw; height: 40vh;">
-            <div class="col-md-4 d-flex" style="width: 35vw;">
-              <img src="${events.image}" class="rounded float-start" alt="Gente en el cine" style="height: 13vw;">
-            </div>
-            <div class="m-2 d-flex">
-              <div class="card-body">
-                <h3 class="card-title  fs-1">${events.name}</h3>
-                <p class="card-text text-end">${events.description}</p>
-                <p class="card-text text-end">$ ${events.price}</p>
-                <p class="card-text text-end">${asistenciaPast}</p>
-              </div>
-            </div>
+    let detailCardOpen = `
+      <div class="card m-5 p-4 d-flex flex-column border-danger-subtle details-img-container" style="width: 50rem;">
+        
+
+          <img src="${events.image}" class="d-flex rounded details-img" alt="Image card of ${events.name}">
+          
+          <div class="card-body">
+            <h3 class="card-title fs-1">${events.name}</h3>
+            <p>${events.description}</p>
+            <p><b>Place:</b> ${events.place}</p>
+            <p><b>Capacity:</b> ${events.capacity}</p>
+            <p>${asistenciaPast}</p>
+            <h4 class="fs-6"><b>Date:</b> ${events.date}</h4>
+            <h4 class="card-text fs-3 m-3"><b>$ ${events.price}</b></h4>
           </div>
-        </div>`;
+          
+        
+      </div>`;
             
     detailContainer.innerHTML += detailCardOpen;
 
